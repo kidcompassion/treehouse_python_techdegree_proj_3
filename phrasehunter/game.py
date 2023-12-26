c1ce55857c706e994f45f6ff6edc19c194bcb80a
Sally Poulsen <sallypoulsen@Sallys-MacBook-Pro.local>
2023-12-26 15:58:43 -0700

    Set up files and imports correctly

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,37 @@
 # Create your Game class logic in here.
+import random
+
+
+
+class Game:
+
+
+
+
+    def __init__(self):
+        self.missed = 0
+        self.phrases = ["Ahoy matey", "Up and away", "Swab the decks", "The ship has sailed", "The train has left the station"]
+        self.active_phrase = None
+        self.guesses = []
+
+
+    def start():
+        pass
+
+    def get_random_phrase(self):
+        # Generate a random index to retrieve from the list of phrases
+        randomizer = random.randrange(0, len(self.phrases))
+        return self.phrases[randomizer]
+
+
+    def welcome():
+        pass
+
+
+    def get_guess():
+        pass
+
+
+    def game_over():
+        pass
+
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,24 @@
 # Create your Phrase class logic here.
+
+
+
+class Phrase:
+
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+        print(self.phrase)
+
+    def display(self):
+        pass
+
+
+    def check_letter(self):
+        pass
+        # if guess exists in phrase
+        #
+
+
+
+    def check_complete():
+        pass
+    
