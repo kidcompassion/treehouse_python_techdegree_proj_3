387f177e7f6a9a28593f8944983604273da044e9
Sally Poulsen <sallypoulsen@Sallys-MacBook-Pro.local>
2024-02-24 20:57:59 -0700

    Adds comments

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -4,7 +4,6 @@ import sys
 
 # Create your Game class logic in here.
 class Game():
-
     def __init__(self):
         self.missed = 0
         self.phrases = [
@@ -35,60 +34,64 @@ class Game():
         self.get_guess()
 
     def get_guess(self):
+        # Flag to indicate whether user inputs incorrect content (int, multi-character, etc).
         input_error = False
 
+        # Check if the last guessed solve the puzzle
         if self.active_phrase.check_complete() == True:
             print("You won, great job!")
             self.game_over()
-             
+
+        # Ask for user guess 
         user_guess = input("What letter would you like to guess? ")
 
+        # INPUT ERROR HANDLING
+        # If user enters more than one letter
         if len(user_guess) > 1:
             print("\n/** Error: One character only, please! **/\n")
             input_error = True
-            #user_guess
-
+        
+        # If user enters a number
         if user_guess.isdigit() == True:
             print("\n/** Error: Enter letters only, please! **/\n")  
             input_error = True
-            #user_guess 
         
+        # If user enters nothing
         if user_guess == "":
             print("\n /** Enter a guess, please! **/ \n")
             input_error = True
-            #user_guess
 
+        # If user enters an empty space
         if user_guess == " ":
             print("\n /** Enter letters only, please! **/ \n")
             input_error = True
-            #user_guess     
     
-
+        # If guess is wrong AND there is no input error, increment error cunter
         if self.active_phrase.check_letter(user_guess) == False and input_error == False:
             self.guesses.append(user_guess)
             self.missed += 1
             print("\nWhoops, you have " + str(self.missed) + " incorrect guesses.\n")
         else:
+            # If guess is correct, just add guessed letter to list
             self.guesses.append(user_guess)
         
+        # If user gets 5 wrong, they lose
         if self.missed >= 5:
             print("\nSorry, that's 5 errors. You lose.\n")
             self.game_over()
+        # otherwise, keep playing
         else:
             self.get_guess()
 
-        
-
     def game_over(self):
-        #print("game_over")
+        # Ask user if they want to play again
         play_again = input("Would you like to play again? Y/N ").lower()
+        # If they do, restart the game
         if play_again == "y":
             self.start()
+        # IF not, quit the game
         elif play_again == "n":
             print("\nOkay, thanks for playing, see you next time!\n")
             sys.exit()
         else:
             print("Sorry, I don't understand that.")
-            
-
-
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -4,36 +4,43 @@ class Phrase():
         self.phrase = phrase.lower()
         self.hidden_phrase = []
         
-
-
     def display(self):
+        # Hidden phrase will hold the hidden phrase (____ ____), and update with each guess
         self.hidden_phrase = []
+        # Generate the hidden version of the phrase, and render spaces appropriately
         for x in self.phrase:
             if x != " ":
                 self.hidden_phrase.append("_")
             else:
                 self.hidden_phrase.append(" ")
+        
         return "".join(self.hidden_phrase)
        
-     # pass user guess
+    
     def check_letter(self, user_guess):
+        # If user guess is correct...
         if user_guess in self.phrase:  
             # If guess is in string, get every instance of its index
             # https://stackoverflow.com/questions/176918/how-to-find-the-index-for-a-given-item-in-a-list
+            
+            # ... find the index for any appearance of the guess
             indexes = [i for i, letter in enumerate(self.phrase) if letter == user_guess]
+            # ...for each index, show the actual letter
             for index in indexes:
                 self.hidden_phrase[index] = user_guess
+            # Flatten the hidden phrase into a string and print it
             print("".join(self.hidden_phrase))
+            # Check whether the word is totally revealed
             self.check_complete()
-            #print("phrase check letter")
+            # Return True so the Game class knows it was a valid guess
             return True
         else:
             # Bubble up the error so Game can track missed guesses
             return False       
 
     def check_complete(self):
+        # If original phrase and hidden phrase match, then puzzle is complete.
         if self.phrase == "".join(self.hidden_phrase):
-            #print("check complete")
             return True
 
 
